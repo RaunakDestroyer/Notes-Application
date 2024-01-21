8259a3f6cdef485fb6629f975b2d29dfa1da41e7
RaunakDestroyer <singhraunak1000@gmail.com>
2024-01-21 16:52:37 +0530

    changes in app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 app.use(express.static("public"));
 
 mongoose.connect(mongoURL);
